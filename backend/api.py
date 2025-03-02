6ddf278f58cd2adf44c8dccd95049f06279765ab
Phionanamugga <phionanamugga15@gmail.com>
2025-03-02 12:40:27 +0100

    modifies back end

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask, request, jsonify
 from chatbot.model import ChatbotModel  # Import chatbot model
 
